de263368ae143b92c537cae6c43353872637b65b
leonardo emil <theflaco56@hotmail.com>
2019-05-29 14:30:15 -0400

    Creando Contexto

---
+++ Parcial1-LeonardoEmil/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace Parcial1_LeonardoEmil.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("Constr") { }
+
+        public DbSet<Productos> Producto { get; set; }
+    }
+}
