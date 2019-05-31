2c146fca32d06e343b55e93410b49591d0e22fd8
leonardo emil <theflaco56@hotmail.com>
2019-05-31 17:31:04 -0400

    metodo Modificar InventarioBLL

--- Parcial1-LeonardoEmil/BLL/InventariosBLL.cs
+++ Parcial1-LeonardoEmil/BLL/InventariosBLL.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Parcial1_LeonardoEmil.Entidades;
 using Parcial1_LeonardoEmil.DAL;
+using System.Data.Entity;
 
 namespace Parcial1_LeonardoEmil.BLL
 {
@@ -19,6 +20,27 @@ namespace Parcial1_LeonardoEmil.BLL
                 if (contexto.Inventario.Add(inventario) != null)
                     paso = contexto.SaveChanges() > 0;
 
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(Inventarios inventario)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(inventario).State = EntityState.Modified;
+                paso = (contexto.SaveChanges() > 0);
             }
             catch(Exception)
             {
