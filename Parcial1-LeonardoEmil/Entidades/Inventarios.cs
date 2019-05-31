f0b6e3dd164a8ce57d2637259d215bd80e43fa98
leonardo emil <theflaco56@hotmail.com>
2019-05-31 17:23:38 -0400

    agregando entidad inventario

---
+++ Parcial1-LeonardoEmil/Entidades/Inventarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_LeonardoEmil.Entidades
+{
+   public class Inventarios
+    {
+        [Key]
+        public int InventarioId { get; set; }
+        public int TotalInventario { get; set; }
+    }
+}
