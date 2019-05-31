80ed988e0238a2ef6fedea40e11cd128fa6a2658
leonardo emil <theflaco56@hotmail.com>
2019-05-31 17:28:24 -0400

    Metodo Guardar inventario

--- Parcial1-LeonardoEmil/BLL/InventariosBLL.cs
+++ Parcial1-LeonardoEmil/BLL/InventariosBLL.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Parcial1_LeonardoEmil.Entidades;
+using Parcial1_LeonardoEmil.DAL;
 
 namespace Parcial1_LeonardoEmil.BLL
 {
    public class InventariosBLL
     {
+        public static bool Guardar(Inventarios inventario)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                if (contexto.Inventario.Add(inventario) != null)
+                    paso = contexto.SaveChanges() > 0;
+
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
     }
 }
