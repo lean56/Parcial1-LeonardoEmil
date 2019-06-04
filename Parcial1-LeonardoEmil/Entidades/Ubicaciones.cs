c256c32052492bf0397d03e29244572342d3e4fb
leonardo emil <theflaco56@hotmail.com>
2019-06-04 11:56:01 -0400

    entidad ubicaciones

---
+++ Parcial1-LeonardoEmil/Entidades/Ubicaciones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_LeonardoEmil.Entidades
+{
+    public class Ubicaciones
+    {
+        [Key]
+        public int UbicacionId { get; set; }
+        public string Descripcion { get; set; }
+
+
+        public Ubicaciones()
+        {
+            UbicacionId = 0;
+            Descripcion = string.Empty;
+
+        }
+    }
+}
